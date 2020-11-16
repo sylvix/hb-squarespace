9e5debecfe08ea65d0c8f6f42ca932307b22285d
Dmitrii Tsyganov <sylvix.dh@gmail.com>
2020-11-16 21:28:23 +0600

    Update test.js

--- test.js
+++ test.js
@@ -6,20 +6,7 @@ var _gsScope = "undefined" != typeof module && module.exports && "undefined" != 
     _ = -1 !== ((_gsScope.navigator || {}).userAgent || "").indexOf("Edge"), C = "DrawSVGPlugin",
     S = String.fromCharCode(103, 114, 101, 101, 110, 115, 111, 99, 107, 46, 99, 111, 109), // greensock.com
     m = String.fromCharCode(47, 114, 101, 113, 117, 105, 114, 101, 115, 45, 109, 101, 109, 98, 101, 114, 115, 104, 105, 112, 47),
-    w = function (e) {
-      for (
-        var t = -1 !== (window ? window.location.href : "").indexOf(String.fromCharCode(103, 114, 101, 101, 110, 115, 111, 99, 107)) // greensock
-          && -1 !== e.indexOf(String.fromCharCode(108, 111, 99, 97, 108, 104, 111, 115, 116)), // localhost
-          r = [
-            S,
-            String.fromCharCode(99, 111, 100, 101, 112, 101, 110, 46, 105, 111),
-            String.fromCharCode(99, 111, 100, 101, 112, 101, 110, 46, 112, 108, 117, 109, 98, 105, 110, 103),
-            String.fromCharCode(99, 111, 100, 101, 112, 101, 110, 46, 100, 101, 118),
-            String.fromCharCode(99, 115, 115, 45, 116, 114, 105, 99, 107, 115, 46, 99, 111, 109),
-            String.fromCharCode(99, 100, 112, 110, 46, 105, 111), String.fromCharCode(103, 97, 110, 110, 111, 110, 46, 116, 118), String.fromCharCode(99, 111, 100, 101, 99, 97, 110, 121, 111, 110, 46, 110, 101, 116), String.fromCharCode(116, 104, 101, 109, 101, 102, 111, 114, 101, 115, 116, 46, 110, 101, 116), String.fromCharCode(99, 101, 114, 101, 98, 114, 97, 120, 46, 99, 111, 46, 117, 107), String.fromCharCode(116, 121, 109, 112, 97, 110, 117, 115, 46, 110, 101, 116), String.fromCharCode(116, 119, 101, 101, 110, 109, 97, 120, 46, 99, 111, 109), String.fromCharCode(116, 119, 101, 101, 110, 108, 105, 116, 101, 46, 99, 111, 109), String.fromCharCode(112, 108, 110, 107, 114, 46, 99, 111), String.fromCharCode(104, 111, 116, 106, 97, 114, 46, 99, 111, 109), String.fromCharCode(119, 101, 98, 112, 97, 99, 107, 98, 105, 110, 46, 99, 111, 109), String.fromCharCode(97, 114, 99, 104, 105, 118, 101, 46, 111, 114, 103), String.fromCharCode(99, 111, 100, 101, 115, 97, 110, 100, 98, 111, 120, 46, 105, 111), String.fromCharCode(115, 116, 97, 99, 107, 98, 108, 105, 116, 122, 46, 99, 111, 109), String.fromCharCode(106, 115, 102, 105, 100, 100, 108, 101, 46, 110, 101, 116)], o = r.length; -1 < --o;) if (-1 !== e.indexOf(r[o])) return !0;
-      
-      return false;
-    }(window ? window.location.host : "");
+    w = true;
 
   function l(e, t, r, o, i, s) {
     return r = (parseFloat(r || 0) - parseFloat(e || 0)) * i, o = (parseFloat(o || 0) - parseFloat(t || 0)) * s, Math.sqrt(r * r + o * o)
